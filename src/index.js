0ee5cc4515d0faeff151cefbee3ed0050de24892
gledsondf <gledsondf@gmail.com>
2021-04-13 10:31:06 -0300

    desafio-rocket-2
    desafio completo e testado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "user not found"});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  const total = user.todos.length;
+  if(total < 10 || user.pro){
+    next();
+  }else {
+    return response.status(403).json({error: "user already has ten or haven't plan pro"});
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error: "user not found"});
+  }
+
+  const validoID = validate(id);
+
+  if(!validoID) {
+    return response.status(400).json({error:"ID not valid"});
+  }
+
+  const idTodoExist = user.todos.find(todo => todo.id === id);
+
+  if(!idTodoExist){
+    return response.status(404).json({error: "todo not found"});
+  }
+
+  request.user = user;
+  request.todo = idTodoExist;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({erro: "User not found"});
+  }
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
